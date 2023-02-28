a8ebfb01c65a044c581dad2f05f8fbe76ac15b9d
KazukiDaido <k10de302222@gmail.com>
2023-02-28 18:13:34 +0900

    Initial Commit

---
+++ Shakespeare/ShakespeareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakespeareApp.swift
+//  Shakespeare
+//
+//  Created by 大道一輝 on 2023/02/28.
+//
+
+import SwiftUI
+
+@main
+struct ShakespeareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
