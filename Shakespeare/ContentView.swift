a4d4f3be5db023ddc26d5886457584bfe45dc849
KazukiDaido <k10de302222@gmail.com>
2023-03-04 14:39:05 +0900

    webから情報を取る。（エラー）

--- Shakespeare/ContentView.swift
+++ Shakespeare/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = "a"
+    @State private var note = ""
+    @State private var age = ""
+    @State private var registerDate = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        Text(name)
+        Text(note)
+        Text(age)
+        Text(registerDate)
+        Button("名前変更ボタン") {
+            pushBotton()
         }
-        .padding()
+    }
+    func pushBotton() {
+        /*var shakespeare =
+        Shakespeare(name: "a", note: "b", age: 12, registerDate: "c")
+        name = shakespeare.name
+        note = shakespeare.note
+        age = "\(shakespeare.age)"
+        registerDate = shakespeare.registerDate
+         */
+        let url = URL(string: "https://umayadia-apisample.azurewebsites.net/api/persons/Shakespeare")!  //URLを生成
+        var request = URLRequest(url: url)               //Requestを生成
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in  //非同期で通信を行う
+            guard let data = data else { return }
+            do {
+                let object = try JSONSerialization.jsonObject(with: data, options: [])  // DataをJsonに変換
+                let jsonData = object.data(using: .utf8)
+                let shakespeare = try! JSONDecoder().decode(Shakespeare.self, from: jsonData!)
+                print(shakespeare)
+            } catch let error {
+                print(error)
+            }
+        }
+        task.resume()
     }
 }
 
---
+++ Shakespeare/Shakespeare.swift
@@ -0,0 +1,15 @@
+//
+//  Shakespeare.swift
+//  Shakespeare
+//
+//  Created by 大道一輝 on 2023/02/28.
+//
+
+import Foundation
+
+struct Shakespeare: Hashable, Codable {
+    var name: String
+    var note: String
+    var age: Int
+    var registerDate: String
+}
