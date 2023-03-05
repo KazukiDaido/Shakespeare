91118e16f5b816ec826c8a27ac8996b0bbb142d4
KazukiDaido <k10de302222@gmail.com>
2023-03-05 12:49:38 +0900

    JsonDecorderを使いたいがエラー発生

--- Shakespeare/ContentView.swift
+++ Shakespeare/ContentView.swift
@@ -25,31 +25,32 @@ struct ContentView: View {
     }
     func pushBotton() {
         /*var shakespeare =
-        Shakespeare(name: "a", note: "b", age: 12, registerDate: "c")
-        name = shakespeare.name
-        note = shakespeare.note
-        age = "\(shakespeare.age)"
-        registerDate = shakespeare.registerDate
+         Shakespeare(name: "a", note: "b", age: 12, registerDate: "c")
+         name = shakespeare.name
+         note = shakespeare.note
+         age = "\(shakespeare.age)"
+         registerDate = shakespeare.registerDate
          */
         let url = URL(string: "https://umayadia-apisample.azurewebsites.net/api/persons/Shakespeare")!  //URLを生成
         var request = URLRequest(url: url)               //Requestを生成
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in  //非同期で通信を行う
             guard let data = data else { return }
+            /*do {
+             let object = try JSONSerialization.jsonObject(with: data, options: [])  // DataをJsonに変換
+             */
             do {
-                let object = try JSONSerialization.jsonObject(with: data, options: [])  // DataをJsonに変換
-                let jsonData = object.data(using: .utf8)
-                let shakespeare = try! JSONDecoder().decode(Shakespeare.self, from: jsonData!)
-                print(shakespeare)
-            } catch let error {
-                print(error)
+                let decoder = JSONDecoder()
+                return try decoder.decode(Shakespeare.self, from: data)
+                print(decoder)
+            } catch {
+                fatalError("Couldn't parse as \(Shakespeare.self):\n\(error)")
             }
         }
         task.resume()
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
