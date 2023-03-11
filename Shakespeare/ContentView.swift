1c66b723eb25652493b2cd253a25a0b2fcbc84a1
KazukiDaido <k10de302222@gmail.com>
2023-03-11 11:50:19 +0900

    DispatchQueue.main.async {                     self.shakespeares = response.data.reversed()                 }

--- Shakespeare/ContentView.swift
+++ Shakespeare/ContentView.swift
@@ -13,8 +13,7 @@ struct ContentView: View {
     @State private var age = ""
     @State private var registerDate = ""
     
-    var shakespeares: [Shakespeare]
-    
+    var shakespeares: [Shakespeare] = []
     
     var body: some View {
         
@@ -23,58 +22,54 @@ struct ContentView: View {
             Button("名前変更ボタン") {
                 pushBotton()
             }
-            List {
-                    Text(name)
-                    }
-                }
-        }
-        func pushBotton() {
-            /*var shakespeare =
-             Shakespeare(name: "a", note: "b", age: 12, registerDate: "c")
-             name = shakespeare.name
-             note = shakespeare.note
-             age = "\(shakespeare.age)"
-             registerDate = shakespeare.registerDate
-             */
-            let url = URL(string: "https://umayadia-apisample.azurewebsites.net/api/persons")!  //URLを生成
-            let request = URLRequest(url: url)//Requestを生成
-            print("--------------1")
-            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in  //非同期で通信を行う
-                guard let data = data else { return }
-                do {
-                    // let object = try JSONSerialization.jsonObject(with: data, options: []) // DataをJsonに変換
-                    // print(object)
-                    print("--------------2")
-                    let decoder = JSONDecoder()
-                    let response = try decoder.decode(Success.self, from: data)
-                    
-                    print(response)
-                
-                
-                    ForEach(0 ..< response.data.count, id: \.self) { index in
-                        name = response.data[index].name
-                        note = response.data[index].note
-                        age = "\(response.data[index].age)"
-                        //registerDate = response.data[index].registerDate
-                    }
-                    
-                    /*
-                     List {
-                     ForEach(0 ..< response.data.count, id: \.self) { index in
-                     Text(response.data[index].name)
-                     }
-                     }
-                     */
-                } catch {
-                    fatalError("Couldn't parse as \(Shakespeare.self):\n\(error)")
-                }
+            Text(shakespeares)
             }
-            task.resume()
-            print("--------------3")
         }
-    }
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
+
+    mutating func pushBotton() {
+        /*var shakespeare =
+         Shakespeare(name: "a", note: "b", age: 12, registerDate: "c")
+         name = shakespeare.name
+         note = shakespeare.note
+         age = "\(shakespeare.age)"
+         registerDate = shakespeare.registerDate
+         */
+        let url = URL(string: "https://umayadia-apisample.azurewebsites.net/api/persons")!  //URLを生成
+        let request = URLRequest(url: url)//Requestを生成
+        print("--------------1")
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in  //非同期で通信を行う
+            guard let data = data else { return }
+            do {
+                // let object = try JSONSerialization.jsonObject(with: data, options: []) // DataをJsonに変換
+                // print(object)
+                print("--------------2")
+                let decoder = JSONDecoder()
+                let response = try decoder.decode(Success.self, from: data)
+                
+                print(response)
+                
+                
+                DispatchQueue.main.async {
+                    self.shakespeares = response.data.reversed()
+                }
+                
+                /*
+                 List {
+                 ForEach(0 ..< response.data.count, id: \.self) { index in
+                 Text(response.data[index].name)
+                 }
+                 }
+                 */
+            } catch {
+                fatalError("Couldn't parse as \(Shakespeare.self):\n\(error)")
+            }
         }
+        task.resume()
+        print("--------------3")
     }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
